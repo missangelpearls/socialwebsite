ff59383790a453aed3042c593703664150431803
Priyanka <missangelpearls@users.noreply.github.com>
2019-05-21 19:20:56 -0400

    Add files via upload

--- middleware/check.js
+++ middleware/check.js
@@ -1,1 +1,14 @@
 
+
+
+var check = function(req, res, next){
+    
+    if(req.user.profile_created == 0){
+        res.redirect('/profile/create');       
+     }else{
+        next();
+    }
+
+}
+
+module.exports = check;
